8194cd30f5bcc302124699ae661b7800ab6168b2
Ruud Andriessen <r.andriessen@ilysian.com>
2016-04-04 15:39:10 +0200

    Added solve parity script, added plot of plato infinitely eat, format update

--- ParityGames/src/paritygames/PGParser.java
+++ ParityGames/src/paritygames/PGParser.java
@@ -10,7 +10,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
